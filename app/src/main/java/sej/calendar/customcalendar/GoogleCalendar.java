789a12bcd9d05f825fd6a5d794ed0c29f0b3d9af
sangeun-jo <dog8536@naver.com>
2021-04-10 02:32:22 +0900

    change google account

---
+++ app/src/main/java/sej/calendar/customcalendar/GoogleCalendar.java
@@ -0,0 +1,102 @@
+package sej.calendar.customcalendar;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.model.Calendar;
+import com.google.api.services.calendar.model.CalendarList;
+import com.google.api.services.calendar.model.CalendarListEntry;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventDateTime;
+import com.google.api.services.calendar.model.Events;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+import static androidx.core.app.ActivityCompat.startActivityForResult;
+
+/**
+ * Created by Administrator on 2017-05-26.
+ */
+
+public class GoogleCalendar {
+
+    private com.google.api.services.calendar.Calendar mService = null;
+
+    private GoogleCalendar(com.google.api.services.calendar.Calendar mService) {
+        this.mService = mService;
+    }
+
+    public static GoogleCalendar build(GoogleAccountCredential mCredential) {
+        HttpTransport transport = AndroidHttp.newCompatibleTransport();
+        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+        com.google.api.services.calendar.Calendar mService = new com.google.api.services.calendar.Calendar.Builder(
+                transport, jsonFactory, mCredential)
+                .setApplicationName("Google Calendar API Android Quickstart")
+                .build();
+
+        return new GoogleCalendar(mService);
+    }
+
+    public String getCalendarID(String calendarTitle) throws IOException {
+        String id = null;
+
+        // Iterate through entries in calendar list
+        String pageToken = null;
+        do {
+            CalendarList calendarList = null;
+            calendarList = mService.calendarList().list().setPageToken(pageToken).execute();
+            List<CalendarListEntry> items = calendarList.getItems();
+            for (CalendarListEntry calendarListEntry : items) {
+                if ( calendarListEntry.getSummary().equals(calendarTitle)) {
+                    id = calendarListEntry.getId();
+                }
+            }
+            pageToken = calendarList.getNextPageToken();
+        } while (pageToken != null);
+
+        return id;
+    }
+
+    private String createCalendar() throws IOException {
+        String ids = getCalendarID("Custom Calendar");
+        if ( ids != null ){
+            return "Calendar already exist";
+        }
+        // 새로운 캘린더 생성
+        com.google.api.services.calendar.model.Calendar calendar = new Calendar();
+        // 캘린더의 제목 설정
+        calendar.setSummary("Custom Calendar");
+        // 캘린더의 시간대 설정
+        //calendar.setTimeZone("Asia/Seoul");
+        // 구글 캘린더에 새로 만든 캘린더를 추가
+        Calendar createdCalendar = mService.calendars().insert(calendar).execute();
+        // 추가한 캘린더의 ID를 가져옴.
+        String calendarId = createdCalendar.getId();
+        // 구글 캘린더의 캘린더 목록에서 새로 만든 캘린더를 검색
+        CalendarListEntry calendarListEntry = mService.calendarList().get(calendarId).execute();
+        // 캘린더의 배경색을 파란색으로 표시  RGB
+        calendarListEntry.setBackgroundColor("#0000ff");
+        // 변경한 내용을 구글 캘린더에 반영
+        CalendarListEntry updatedCalendarListEntry =
+                mService.calendarList()
+                        .update(calendarListEntry.getId(), calendarListEntry)
+                        .setColorRgbFormat(true)
+                        .execute();
+        // 새로 추가한 캘린더의 ID를 리턴
+        return "Custom Calendar Created";
+    }
+
+}
---
+++ app/src/main/java/sej/calendar/customcalendar/ui/GoogleCalendarActivity.java
@@ -0,0 +1,218 @@
+package sej.calendar.customcalendar.ui;
+
+import android.Manifest;
+import android.accounts.AccountManager;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAuthIOException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+
+import java.util.List;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class GoogleCalendarActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+
+    private int viewId;
+
+    static final int REQUEST_ACCOUNT_PICKER = 1000;
+    static final int REQUEST_AUTHORIZATION = 1001;
+    static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
+
+    CalendarTaskListener calendarTaskListener = null;
+
+    public interface CalendarTaskListener {
+        void onAvailableCalendarTask(int viewId);
+        void onErrorCalendarTask(String message);
+    }
+
+    public void setCalendarTaskListener(CalendarTaskListener calendarTaskListener) {
+        this.calendarTaskListener = calendarTaskListener;
+    }
+
+    GoogleAccountCredential mCredential = null;
+
+    public void setCredential(GoogleAccountCredential mCredential) {
+        this.mCredential = mCredential;
+    }
+
+    public void setViewId(int viewId) {
+        this.viewId = viewId;
+    }
+
+    @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
+    void chooseAccount() {
+        System.out.println("chooseAccount() 실행");
+        if (EasyPermissions.hasPermissions(
+                this, Manifest.permission.GET_ACCOUNTS)) {
+            startActivityForResult(mCredential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
+        } else {
+            // Request the GET_ACCOUNTS permission via a user dialog
+            EasyPermissions.requestPermissions(
+                    this,
+                    "This app needs to access your Google account (via Contacts).",
+                    REQUEST_PERMISSION_GET_ACCOUNTS,
+                    Manifest.permission.GET_ACCOUNTS);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case REQUEST_GOOGLE_PLAY_SERVICES:
+                if (resultCode != RESULT_OK) {
+                    if(calendarTaskListener != null)
+                        calendarTaskListener.onErrorCalendarTask("This app requires Google Play Services. Please install " +
+                                "Google Play Services on your device and relaunch this app.");
+                } else {
+                    if(calendarTaskListener != null)
+                        isCalendarTaskAvailable(viewId);
+                }
+                break;
+            case REQUEST_ACCOUNT_PICKER:
+                System.out.println("REQUEST_ACCOUNT_PICKER 실행");
+                if (resultCode == RESULT_OK && data != null && data.getExtras() != null) {
+                    String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        System.out.println(accountName  + " 계정 선택됨");
+                        mCredential.setSelectedAccountName(accountName);
+                        SharedPreferences settings = getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.putString("savedAccount", accountName);
+                        editor.apply();
+                        if (calendarTaskListener != null) {
+                            isCalendarTaskAvailable(viewId);
+                        }
+                    }
+
+
+                }
+                break;
+            case REQUEST_AUTHORIZATION:
+                if (resultCode == RESULT_OK) {
+                    if(calendarTaskListener != null)
+                        isCalendarTaskAvailable(viewId);
+                }
+                else {
+                    if(calendarTaskListener != null)
+                        calendarTaskListener.onErrorCalendarTask("권한 설정을 허용해야 이용 가능합니다!");
+                }
+                break;
+        }
+    }
+
+    //캘린더 작업이 가능한지 조건 따지는 애
+    protected final void isCalendarTaskAvailable(int viewId) {
+        System.out.println("isCalendarTaskAvailable 도착");
+        this.viewId = viewId;
+        if (!isGooglePlayServicesAvailable()) { //1. 구글 플레이 서비스 사용가능한지 확인
+            acquireGooglePlayServices();
+        } else if (mCredential.getSelectedAccountName() == null) { //2. 계정 선택 안됨
+            chooseAccount();
+            //calendarTaskListener.onErrorCalendarTask("먼저 구글 계정을 선택해주세요");
+        } else if (!isDeviceOnline()) { //3. 온라인 상태 확인
+            if(calendarTaskListener != null)
+                calendarTaskListener.onErrorCalendarTask("No network connection available.");
+        } else { // 캘린더 작업 가능!!
+            if(calendarTaskListener != null)
+                System.out.println("캘린더 작업 가능!");
+                calendarTaskListener.onAvailableCalendarTask(viewId);
+        }
+    }
+
+    private boolean isDeviceOnline() {
+        ConnectivityManager connMgr =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+
+    private boolean isGooglePlayServicesAvailable() {
+        GoogleApiAvailability apiAvailability =
+                GoogleApiAvailability.getInstance();
+        final int connectionStatusCode =
+                apiAvailability.isGooglePlayServicesAvailable(this);
+        return connectionStatusCode == ConnectionResult.SUCCESS;
+    }
+
+    /**
+     * Attempt to resolve a missing, out-of-date, invalid or disabled Google
+     * Play Services installation via a user dialog, if possible.
+     */
+    private void acquireGooglePlayServices() {
+        GoogleApiAvailability apiAvailability =
+                GoogleApiAvailability.getInstance();
+        final int connectionStatusCode =
+                apiAvailability.isGooglePlayServicesAvailable(this);
+        if (apiAvailability.isUserResolvableError(connectionStatusCode)) {
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
+        }
+    }
+
+
+    void showGooglePlayServicesAvailabilityErrorDialog(
+            final int connectionStatusCode) {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        Dialog dialog = apiAvailability.getErrorDialog(
+                this,
+                connectionStatusCode,
+                REQUEST_GOOGLE_PLAY_SERVICES);
+        dialog.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(
+                requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+    }
+
+    void handleCommonThrowable(Throwable throwable) {
+        if (throwable != null) {
+            if (throwable instanceof GooglePlayServicesAvailabilityIOException) {
+                showGooglePlayServicesAvailabilityErrorDialog(
+                        ((GooglePlayServicesAvailabilityIOException) throwable)
+                                .getConnectionStatusCode());
+            } else if (throwable instanceof UserRecoverableAuthIOException) {
+                startActivityForResult(
+                        ((UserRecoverableAuthIOException) throwable).getIntent(),
+                        REQUEST_AUTHORIZATION);
+            } else if (throwable instanceof GoogleAuthIOException) {
+                //
+            } else {
+                if(calendarTaskListener != null)
+                    calendarTaskListener.onErrorCalendarTask("The following error occurred:\n" + throwable.getMessage());
+            }
+        } else {
+            if(calendarTaskListener != null)
+                calendarTaskListener.onErrorCalendarTask("Request cancelled.");
+        }
+    }
+}
